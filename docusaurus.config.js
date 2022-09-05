37681878dc5fd4d9b5ea87a29f7b66b7446fa56b
Matthew O'Connell <totallyterrific@tutanota.com>
2022-09-05 16:31:09 +0200

    feat: add solidity API docs; upgrade docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Hypercerts",
   tagline: "Impact in hyperspace",
-  url: "https://protocol.github.io/hypercerts-docs/",
+  url: "https://protocol.github.io/",
   baseUrl: "/hypercerts-docs/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -27,6 +27,19 @@ const config = {
     defaultLocale: "en",
     locales: ["en"],
   },
+
+  plugins: [
+    [
+      "docusaurus-plugin-remote-content",
+      {
+        // options here
+        name: "contracts", // used by CLI, must be path safe
+        sourceBaseUrl: "https://raw.githubusercontent.com/protocol/hypercerts-protocol/main/docs/", // the base url for the markdown (gets prepended to all of the documents when fetching)
+        outDir: "docs/contracts", // the base directory to output to.
+        documents: ["index.md"], // the file names to download
+      },
+    ],
+  ],
 
   presets: [
     [
